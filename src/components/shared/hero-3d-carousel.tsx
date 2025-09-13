54fdf0747ee232d31ad8cd33458c8f8943571fda
Facundo Canepa <facundocanepa40@gmail.com>
2025-09-13 21:20:01 +0000

    Quiero que se vea la palabra VESTIGIO con este estilo  (_for element sec

--- src/components/shared/hero-3d-carousel.tsx
+++ src/components/shared/hero-3d-carousel.tsx
@@ -1,17 +1,18 @@
 
-export const Hero3DCarousel = () => {
+import * as React from 'react';
+
+export const Hero3DCarousel = ({ children }: { children?: React.ReactNode }) => {
   return (
-    <div className="card-3d">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
+    <>
+      <div className="absolute inset-0 bg-black/30 backdrop-blur-sm z-10" />
+      <div className="card-3d">
+        {[...Array(10)].map((_, i) => (
+          <div key={i} className="card-item" />
+        ))}
+      </div>
+      <div className="relative z-20">
+        {children}
+      </div>
+    </>
   );
 };
