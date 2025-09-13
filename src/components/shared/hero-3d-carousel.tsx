fa14c2fe7945d873db63f0166e7ad083488e61d4
Facundo Canepa <facundocanepa40@gmail.com>
2025-09-13 21:23:22 +0000

    Quiero que cada uno de esto , no sea un contenedor con un fondo gris sin

--- src/components/shared/hero-3d-carousel.tsx
+++ src/components/shared/hero-3d-carousel.tsx
@@ -2,11 +2,16 @@
 import * as React from 'react';
 
 export const Hero3DCarousel = () => {
+  const word = "VESTIGIO";
+  const letters = word.split('');
+
   return (
     <>
       <div className="card-3d">
-        {[...Array(10)].map((_, i) => (
-          <div key={i} className="card-item" />
+        {letters.map((letter, i) => (
+          <div key={i} className="card-item">
+            {letter}
+          </div>
         ))}
       </div>
       <div className="relative z-20">
