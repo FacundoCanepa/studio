06aa764bae9ef79119bf3487a401edfe163bc191
Facundo Canepa <facundocanepa40@gmail.com>
2025-09-13 21:39:05 +0000

    Agrega unos iconos en el fondo haciendo animaciones  (_for element secti

--- src/components/shared/floating-icons-hero.tsx
+++ src/components/shared/floating-icons-hero.tsx
@@ -1,21 +1,40 @@
 
 import * as React from 'react';
+import { Watch, Feather, Shirt, Scissors, Gem } from 'lucide-react';
 
 export const FloatingIconsHero = () => {
   const letters = 'VESTIGIO'.split('');
+  const icons = [
+    { Icon: Watch, className: 'top-[10%] left-[10%] w-8 h-8 animation-delay-[-2s] duration-[8s]' },
+    { Icon: Feather, className: 'top-[20%] right-[15%] w-10 h-10 animation-delay-[-4s] duration-[10s]' },
+    { Icon: Shirt, className: 'bottom-[15%] left-[20%] w-9 h-9 animation-delay-[-6s] duration-[9s]' },
+    { Icon: Scissors, className: 'bottom-[25%] right-[25%] w-7 h-7 animation-delay-[-1s] duration-[11s]' },
+    { Icon: Gem, className: 'top-[50%] left-[30%] w-6 h-6 animation-delay-[-5s] duration-[7s]' },
+  ];
 
   return (
-    <div className="wrapper-grid">
-      {letters.map((letter, index) => (
-        <div key={index} className="cube">
-          <div className="face face-front">{letter}</div>
-          <div className="face face-back"></div>
-          <div className="face face-right"></div>
-          <div className="face face-left"></div>
-          <div className="face face-top"></div>
-          <div className="face face-bottom"></div>
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="absolute inset-0 z-0 overflow-hidden">
+        {icons.map(({ Icon, className }, index) => (
+          <Icon
+            key={index}
+            className={`icon-float absolute text-foreground/10 ${className}`}
+            strokeWidth={1.5}
+          />
+        ))}
+      </div>
+      <div className="relative z-10 wrapper-grid">
+        {letters.map((letter, index) => (
+          <div key={index} className="cube">
+            <div className="face face-front">{letter}</div>
+            <div className="face face-back"></div>
+            <div className="face face-right"></div>
+            <div className="face face-left"></div>
+            <div className="face face-top"></div>
+            <div className="face face-bottom"></div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
