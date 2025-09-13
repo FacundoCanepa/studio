426a8379f96438448edbf0c838a92f20f85936fa
Facundo Canepa <facundocanepa40@gmail.com>
2025-09-13 21:40:05 +0000

    Agrega unos 4 iconos mas ademas de lo que ya etsan y quiero que se mueva

--- src/components/shared/floating-icons-hero.tsx
+++ src/components/shared/floating-icons-hero.tsx
@@ -1,6 +1,6 @@
 
 import * as React from 'react';
-import { Watch, Feather, Shirt, Scissors, Gem } from 'lucide-react';
+import { Watch, Feather, Shirt, Scissors, Gem, Sparkles, Palette, Crown, Glasses } from 'lucide-react';
 
 export const FloatingIconsHero = () => {
   const letters = 'VESTIGIO'.split('');
@@ -10,6 +10,10 @@ export const FloatingIconsHero = () => {
     { Icon: Shirt, className: 'bottom-[15%] left-[20%] w-9 h-9 animation-delay-[-6s] duration-[9s]' },
     { Icon: Scissors, className: 'bottom-[25%] right-[25%] w-7 h-7 animation-delay-[-1s] duration-[11s]' },
     { Icon: Gem, className: 'top-[50%] left-[30%] w-6 h-6 animation-delay-[-5s] duration-[7s]' },
+    { Icon: Sparkles, className: 'top-[15%] left-[40%] w-8 h-8 animation-delay-[-8s] duration-[12s]' },
+    { Icon: Palette, className: 'bottom-[10%] right-[10%] w-10 h-10 animation-delay-[-3s] duration-[8s]' },
+    { Icon: Crown, className: 'top-[5%] right-[35%] w-9 h-9 animation-delay-[-7s] duration-[10s]' },
+    { Icon: Glasses, className: 'bottom-[40%] left-[5%] w-11 h-11 animation-delay-[-9s] duration-[9s]' },
   ];
 
   return (
