c737d2f271a12735f15016a056496ef6b2988bd6
Facundo Canepa <facundocanepa40@gmail.com>
2025-09-13 21:21:53 +0000

    saca este div , quiero que se vea lo de abajo (_for element div_)

--- src/components/shared/hero-3d-carousel.tsx
+++ src/components/shared/hero-3d-carousel.tsx
@@ -4,7 +4,6 @@ import * as React from 'react';
 export const Hero3DCarousel = () => {
   return (
     <>
-      <div className="absolute inset-0 bg-black/30 backdrop-blur-sm z-10" />
       <div className="card-3d">
         {[...Array(10)].map((_, i) => (
           <div key={i} className="card-item" />
